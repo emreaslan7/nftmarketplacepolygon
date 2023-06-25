3d30ccb6e9497d5d7089cbf5fffc0871e8cce593
Emre Aslan <emreaslan7@icloud.com>
2023-06-25 20:42:28 +0300

    setup stuff

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+//  unused configuration commented out for now
+    // mumbai: {
+    //   url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
+    //   // accounts: [process.env.privateKey]
+    // }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
