7b3ffea1e90cad1507ab1f07e1c5561bcc29113f
Emre Aslan <emreaslan7@icloud.com>
2023-06-26 08:54:58 +0300

    added NFT contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,5 +5,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Lock {
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("Metaverse Tokens","METT"){
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+        return newTokenId;
+    }
 }
